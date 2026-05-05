a9ce0f8ba14d6fd2de60efe4cb6099f2a90900ea
Karl <karlkheyfer213@gmail.com>
2026-05-06 08:59:07 +1000

    Fix Stripe build error

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -1,9 +1,8 @@
-import Stripe from 'stripe'
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
-
 export async function POST(request) {
   try {
+    const Stripe = (await import('stripe')).default
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       line_items: [
